9d5f82a2242fe06f674f33eac3420146af34b5a0
Hisoka <hyosoka187@gmail.com>
2015-11-03 09:40:10 +0700

    first commit..... :)

---
+++ NancyApplication1/Modules/HomeModuleClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace NancyApplication1.Modules
+{
+    public class HomeModuleClass : NancyModule
+    {
+        public HomeModuleClass()
+        {
+            Get["/"] = param =>
+            {
+                return View["ViewPage1"];
+            };
+        }
+    }
+}
